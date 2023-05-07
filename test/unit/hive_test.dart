b6d54adc528e04431cdf1d36add5255560d208ee
Makhosandile Ndondo <makhosi@Makhosandiles-MacBook-Air.local>
2023-05-07 22:33:50 +0200

    Sun May  7 22:33:50 SAST 2023 - Make failing tests.

--- test/unit/hive_test.dart
+++ test/unit/hive_test.dart
@@ -1,11 +1,5 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-import 'package:card_security_system/main.dart';
 import 'package:card_security_system/models/banned_countries.dart';
+import 'package:card_security_system/models/boxes.dart';
 import 'package:card_security_system/models/card.dart';
 import 'package:card_security_system/models/theme.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -31,11 +25,35 @@ void main() {
   tearDown(() async {
     await tearDownTestHive();
   });
-  testWidgets('App start up process', (WidgetTester tester) async {
-    // // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
 
-    // fins a placeholder component
-    expect(find.text("0000000000000000"), findsOneWidget);
+  group("Test all Hive models", () {
+    test("toggle user theme", () {
+      var userTheme = AppTheme();
+
+      /// initial value should be null
+      expect(userTheme.theme, null);
+
+      // /// set theme to light
+
+      userTheme.saveData("light");
+
+      expect(userTheme.theme, "light");
+
+      // reset theme to dark mode
+      userTheme.saveData("dark");
+
+      expect(userTheme.theme, "dark");
+
+      /// the app should have one instace of user theme
+      var themeBox = Boxes.getThemeData();
+      expect(themeBox.values.length, 1);
+
+      ///theme value should be "dark" | "light" ONLY
+      expect(() => userTheme.saveData("invalidThemeValue"), throwsException);
+    });
+
+    test("add, update and delete a card", () => {});
+
+    test("add and remove countries", () => {});
   });
 }
